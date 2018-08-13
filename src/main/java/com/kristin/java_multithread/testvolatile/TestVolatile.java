156448196185a8559b05cf281415a7aa93f73b8e
Lee0526 <3215758767@qq.com>
2018-08-13 15:50:04 +0800

    java jit

--- src/main/java/com/kristin/java_multithread/testvolatile/TestVolatile.java
+++ src/main/java/com/kristin/java_multithread/testvolatile/TestVolatile.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 /**
  * @author Kristin
  * @since 2018/8/13 15:37
+ * 下载hsfis插件: https://sourceforge.net/projects/fcml/files/fcml-1.1.1/hsdis-1.1.1-win32-amd64.zip/download
+ * 将插件放到Java\jdk1.8.0_121\jre\bin\server\目录下
+ * 配置VM : -server -Xcomp -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -XX:CompileCommand=compileonly,*TestVolatile.test
  **/
 public class TestVolatile {
     public volatile Singleton singleton;
