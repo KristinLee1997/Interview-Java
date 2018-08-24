c56dfdf4b0d0b7a09541e39526ba2bbef900b479
Lee0526 <3215758767@qq.com>
2018-08-24 10:41:21 +0800

    笔试题

---
+++ src/main/java/com/kristin/笔试题/keep/Main2.java
@@ -0,0 +1,11 @@
+package com.kristin.笔试题.keep;
+
+/**
+ * @author Kristin
+ * @since 2018/8/23 22:07
+ **/
+public class Main2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/kristin/笔试题/keep/Main3.java
@@ -0,0 +1,24 @@
+package com.kristin.笔试题.keep;
+
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int[] num = new int[100];
+        int[] table = new int[65535];
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            for (int i = 0; scan.hasNext(); i++) {
+                num[i] = scan.nextInt();
+            }
+            for (int i = 0; i < num.length; i++) {
+                table[i]++;
+            }
+            for (int i = 0; i < table.length; i++) {
+                if (table[i] >= num.length / 2) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
