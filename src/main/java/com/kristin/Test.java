cccff1f22c3ddf6d94520ebf0ceb3eaf1abc394f
Lee0526 <3215758767@qq.com>
2018-08-27 10:17:08 +0800

    哲学家进餐

--- src/main/java/com/kristin/Test.java
+++ src/main/java/com/kristin/Test.java
@@ -1,24 +1,32 @@
 package com.kristin;
 
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @author Kristin
  * @since 2018/7/27 23:09
  **/
+
 public class Test {
-    public static void main(String[] args) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        System.out.println(map.get(1));
-        if(map.get(1)==null){
-            map.put(1,1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine().toString();
+        String str2 = br.readLine().toString();
+        String res = get(str1, str2);
+        System.out.println(res);
+    }
+
+    public static String get(String str1, String str2) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < str1.length(); i++) {
+            char c = str1.charAt(i);
+            if (str2.indexOf(c) == -1) {
+                res.append(c);
+            }
         }
-        System.out.println(map.get(1));
-        if(map.get(1)==null){
-            map.put(1,1);
-        }else{
-            map.put(1,map.get(1));
-        }
+        return res.toString();
     }
 }
---
+++ src/main/java/com/kristin/java_multithread/deadlock/Philosopher.java
@@ -0,0 +1,14 @@
+package com.kristin.java_multithread.deadlock;
+
+/**
+ * @author Kristin
+ * @since 2018/8/27 9:51
+ **/
+
+public interface Philosopher {
+    void think();
+
+    void eat();
+
+    void start();
+}
