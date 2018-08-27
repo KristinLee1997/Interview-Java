17bec4149ee620405e1eae8916aff72b65d7da6b
Lee0526 <3215758767@qq.com>
2018-08-27 10:20:51 +0800

    哲学家进餐问题

---
+++ src/main/java/com/kristin/java_multithread/philosophers/PerThread.java
@@ -0,0 +1,68 @@
+package com.kristin.java_multithread.philosophers;
+
+/**
+ * @author Kristin
+ * @since 2018/8/27 10:18
+ * 哲学家线程
+ **/
+
+public class PerThread extends Thread {
+
+    private static int[] chopstick = { 1, 1, 1, 1, 1 };
+    private int i;
+
+    public PerThread(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (chopstick) {  //若注释此行，打开下行，不同步，5个per只拿到左筷子
+            //{
+            eat(this.getName());
+
+            think(this.getName());
+        }
+
+    }
+
+    private void think(String name) {
+        chopstick[i] = 1;
+        chopstick[(i + 1) % 5] = 1;
+        System.out.println("per"+name+" is thinking...");
+
+    }
+
+    private void eat(String string) {
+
+        while (true) {
+
+            if (chopstick[i] != 0) {
+                chopstick[i]--;
+                System.out.println("per" + this.getName()
+                        + " got left chopstick.");
+                break;
+            }
+
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            if (chopstick[(i + 1) % 5] != 0) {
+                chopstick[(i + 1) % 5]--;
+                System.out.println("per" + this.getName()
+                        + " got right chopstick.");
+                break;
+            }
+
+        }
+        System.out.println("per" + string + " is eating...");
+    }
+}
+
