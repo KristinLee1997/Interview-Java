277e85f5d79cf1a800fd41d74959ee009af9a086
Lee0526 <3215758767@qq.com>
2018-08-31 20:37:17 +0800

    leetcode题

---
+++ src/main/java/com/kristin/leetcode/动态规划/不同路径/Solution.java
@@ -0,0 +1,12 @@
+package com.kristin.leetcode.动态规划.不同路径;
+
+/**
+ * @author Kristin
+ * @since 2018/8/31 20:36
+ **/
+public class Solution {
+    public int uniquePaths(int m, int n) {
+
+        return -1;
+    }
+}
