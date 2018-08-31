9be7e57d4338a606626570bd7852dc383523230e
Lee0526 <3215758767@qq.com>
2018-08-31 09:38:35 +0800

    leetcode题

--- src/main/java/com/kristin/leetcode/动态规划/整数拆分/Solution.java
+++ src/main/java/com/kristin/leetcode/动态规划/整数拆分/Solution.java
@@ -5,4 +5,20 @@ package com.kristin.leetcode.动态规划.整数拆分;
  * @since 2018/8/31 8:56
  **/
 public class Solution {
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 2;
+        dp[3] = 3;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2] * 2, dp[i - 3] * 3);
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().integerBreak(6));
+    }
 }
